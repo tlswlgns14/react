30055082d597b9593a91b757a9f4ba7c04e69405
tlswlgns14 <tlswlgns14@minigate.net>
2023-12-13 17:52:30 +0900

    no message

--- src/AppJSX.jsx
+++ src/AppJSX.jsx
@@ -1,11 +1,11 @@
-import "./App.css";
+import './App.css';
 
 function AppJSX() {
-  const name = "엘리";
-  const list = ["우유", "딸기", "바나나", "요거트"];
+  const name = '엘리';
+  const list = ['우유', '딸기', '바나나', '요거트'];
   return (
     <>
-      <h1 className="orange">{`Hello!${name}`}</h1>
+      <h1 className='orange'>{`Hello! ${name}`}</h1>
       <h2>Hello!</h2>
       <p>{name}</p>
       <ul>
@@ -14,9 +14,9 @@ function AppJSX() {
         ))}
       </ul>
       <img
-        style={{ width: "200px", height: "200px" }}
-        src="https://images.unsplash.com/photo-1700324022450-f143742da6c1?q=80&w=1286&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt=""
+        style={{ width: '200px', height: '200px' }}
+        src='https://images.unsplash.com/photo-1661956601349-f61c959a8fd4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80'
+        alt='nature'
       />
     </>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import Avater from "./Avatar";
+import React from 'react';
+import Avartar from './Avartar';
 
-export default function profile({ image, name, title, isNew }) {
+export default function Profile({ image, name, title, isNew }) {
   return (
-    <div className="profile">
-      <Avater image={image} isNew={isNew} />
+    <div className='profile'>
+      <Avartar image={image} isNew={isNew} />
       <h1>{name}</h1>
       <p>{title}</p>
     </div>
