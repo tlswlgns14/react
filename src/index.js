e11bc671b72ba6c8c197fd30a8032270415f7105
tlswlgns14 <tlswlgns14@minigate.net>
2023-12-13 18:29:22 +0900

    no message

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import AppMentor from './AppMentor';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import AppMentors from "./AppMentors";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppMentor />
+    <AppMentors />
   </React.StrictMode>
 );
 
