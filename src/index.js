c1a18e936b26c8af4cba02b9b8f15908eff20cb4
tlswlgns14 <tlswlgns14@minigate.net>
2023-12-20 18:28:44 +0900

    dd

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import AppCard from "./AppCard";
+import AppProducts from "./basic/AppProducts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppCard />
+    <AppProducts />
   </React.StrictMode>
 );
 
